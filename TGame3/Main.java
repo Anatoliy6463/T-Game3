d5f1f12a257ce1d230d86091b57fdba60e169bdc
Anatoliy6463 <120381885+Anatoliy6463@users.noreply.github.com>
2023-11-20 23:15:23 +0300

    Update Main.java

--- TGame3/Main.java
+++ TGame3/Main.java
@@ -1,18 +1,57 @@
 /*MAIN FILE
  * Made by Anatoliy6463 on 20.11.2023
  * Version 0.0.1
+ * Protected with Apache License 2.0
 */
 package TGame3;
 import java.util.Scanner;
 public class Main {
     static int X, Y = 1;
-    static int armor, sword = 0;
     static int hp = 100;
     static int hunger = 0;
     static int persiki = 0;
+    static int nektarin = 0;
     static char d = '\0';
     static String line = "\0";
+    static int money = 100;
+    static boolean nektarin_effect, dragonTame = false;
     static Scanner sc = new Scanner(System.in);
+    static int k = 1;
+    static int sword = 0;
+    static int dragonhp = 1000;
+    static int effectK;
+    public static void saturation() {
+        if (nektarin_effect == false && hunger == 0) {
+            System.out.println("Вы сыты!");
+            hp = 100;
+        }
+        if (nektarin_effect == true && hunger == 0) {
+            hp = 150;
+            System.out.println("Вы сыты!");
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Загрузка...");
+        engine();
+    }
+    public static void saving() {
+        System.out.println("Загрузка сохранений");
+        X = Save.X;
+        Y = Save.Y;
+        hp = Save.hp;
+        hunger = Save.hunger;
+        persiki = Save.persiki;
+        nektarin = Save.nektarin;
+        d = Save.d;
+        line = Save.line;
+        money = Save.money;
+        nektarin_effect = Save.nektarin_effect;
+        dragonTame = Save.dragonTame;
+        k = Save.k;
+        sword = Save.sword;
+        dragonhp = Save.dragonhp;
+        effectK = Save.effectK;
+    }
     public static void controller() {
         if (d == 'w') {
             Y++;
@@ -26,20 +65,43 @@ public class Main {
         if (d == 'd') {
             X++;
         }
-        if (d == 'e') {
+        if (d == 'e' && nektarin == 0) {
             persiki--;
             hunger -= 20;
             if (hunger < 0) hunger = 0;
+        }
+        if (d == 'e' && nektarin > 0) {
+            nektarin--;
+            hunger -= 20;
+            if (hunger < 0) hunger = 0;
+            hp = 150;
+            nektarin_effect = true;
+            effectK = k;
         }
         if (d == 'p') {
             System.out.println("У вас " + persiki + "персиков");
         }
         if (d == 'q') {
+            Save.save();
             System.exit(0);
+        }
+        if (d == 'f') {
+            Save.save();
+        }
+        if (d == 'r') {
+            saving();
+            if (X == Save.X && hp == Save.hp && d == Save.d && money == Save.money) {
+                System.out.println("Загрузка прошла успешно");
+            }
         }
     }
     public static void shop() {
-        System.out.println("Магазин закрыт до выхода обновления v0.0.2");
+        System.out.println("Добро пожаловать в магазин! Чтобы купить нектарин, нажмите T, чтобы купить меч, нажмите O");
+        line = sc.nextLine();
+        if (line.equalsIgnoreCase("T")) {
+            nektarin++;
+            money -= 10;
+        }
     }
     public static void engine() {
         while (d != 'q') {
@@ -52,6 +114,28 @@ public class Main {
             }
             if (X == 15 && Y == 5) {
                 shop();
+            }
+            k++;
+            if (X >= 20 && X <= 30 && Y >= 20 && Y <= 30 && dragonhp > 0 && dragonTame != true) {
+                System.out.println("Здесь живёт дракон!");
+                hp -= 5;
+                line = sc.nextLine();
+                if (line.equalsIgnoreCase("attack")) {
+                    if (sword == 0) {
+                        dragonhp -= 10;
+                    }
+                    if (sword == 1) {
+                        dragonhp -= 30;
+                    }
+                }
+                if (line.equalsIgnoreCase("tame")) {
+                    if (nektarin <= 10) {
+                        System.out.println("У вас недостаточно нектаринов для приручения дракона");
+                    }
+                    if (nektarin > 10) {
+                        dragonTame = true;
+                    }
+                }
             }
         }
     }
